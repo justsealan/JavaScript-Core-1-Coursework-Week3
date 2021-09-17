bab8996dd2ddabf0d893e3d8d834df771be9f3fb
justsealan <justsealan@gmail.com>
2021-09-17 16:45:33 +0100

    Complete exercise F

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,22 @@
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+function fizzbuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz';
+  } else if (num % 3 === 0) {
+    return 'Fizz';
+  } else if (num % 5 === 0) {
+    return 'Buzz';
+  } else {
+    return num;
+  }
+}
+
+arr.forEach(function(num) {
+  console.log(fizzbuzz(num));
+});
+
 /* EXPECTED OUTPUT */
 
 /*
