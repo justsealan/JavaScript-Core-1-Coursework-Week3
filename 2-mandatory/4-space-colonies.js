716360b9c0763bedb29732d19f497b9ce4df30e8
justsealan <justsealan@gmail.com>
2021-09-23 22:01:54 +0100

    Complete mandatory 4

--- 2-mandatory/4-space-colonies.js
+++ 2-mandatory/4-space-colonies.js
@@ -15,7 +15,9 @@
   
 */
 
-function getSettlers() {}
+function getSettlers(arr) {
+  return arr.filter(name => name.startsWith("A") && name.endsWith("family"));
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
